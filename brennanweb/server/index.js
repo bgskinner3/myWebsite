4eef3203b196acf2ec52c4b281685042a0255da9
Brennan Skinner <bgskinner3@gmail.com>
2022-05-03 18:17:42 -0400

    chnged package for heroku

--- brennanweb/server/index.js
+++ brennanweb/server/index.js
@@ -5,7 +5,7 @@ const { db, User } = require('./db');
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const { graphqlUploadExpress } = require('graphql-upload');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const cors = require('cors');
 require('dotenv').config();
 
