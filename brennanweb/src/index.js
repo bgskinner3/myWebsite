aec2bdd1c6a72e4981bb08f53c3fef206734cbad
Brennan Skinner <bgskinner3@gmail.com>
2022-05-03 19:09:44 -0400

    https not http

--- brennanweb/src/index.js
+++ brennanweb/src/index.js
@@ -39,7 +39,7 @@ import { createUploadLink } from 'apollo-upload-client';
 
 const jwtAuth = process.env.REACT_APP_JWT_SECRET;
 const httpLink = createUploadLink({
-  uri: 'http://brennanskinner.herokuapp/graphql',
+  uri: 'https://brennanskinner.herokuapp/graphql',
 });
 const authLink = new ApolloLink((operation, forward) => {
   operation.setContext(({ headers }) => {
