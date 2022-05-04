07bb97a6966891aacbc908ddd9ce29f86c452eab
Brennan Skinner <bgskinner3@gmail.com>
2022-05-04 15:28:38 -0400

    changed seever port

--- brennanweb/server/index.js
+++ brennanweb/server/index.js
@@ -5,7 +5,7 @@ const { db, User } = require('./db');
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const { graphqlUploadExpress } = require('graphql-upload');
-const port = process.env.PORT || 4000;
+const port = "https://brennanskinner.herokuapp.com/graphql";
 const cors = require('cors');
 require('dotenv').config();
 
