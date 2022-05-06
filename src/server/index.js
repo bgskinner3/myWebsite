80fd8937e96cd0e5ae543a5896bd47114196d38e
Brennan Skinner <bgskinner3@gmail.com>
2022-05-06 19:32:50 -0400

    connection attempt deployment

--- src/server/index.js
+++ src/server/index.js
@@ -51,7 +51,7 @@ const startServer = async () => {
 
   app.use(express.static(path.join(__dirname, '../../build')));
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../build/index.html'));
   });
   app.use(errorHandler);
   server.applyMiddleware({ path: '/graphql', app });
