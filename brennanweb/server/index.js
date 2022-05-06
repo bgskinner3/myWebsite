91aefca55e8431ec9ead317d4f3517864b24dc9b
Brennan Skinner <bgskinner3@gmail.com>
2022-05-05 23:47:07 -0400

    re-ran build

--- brennanweb/server/index.js
+++ brennanweb/server/index.js
@@ -42,7 +42,7 @@ const startServer = async () => {
   app.use(graphqlUploadExpress());
   app.use(cors());
   app.use(express.static(path.join(__dirname, '../build')));
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
   });
   server.applyMiddleware({ app });
