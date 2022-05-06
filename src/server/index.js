857f17f5c8c4684b37af057095d2088855ea045c
Brennan Skinner <bgskinner3@gmail.com>
2022-05-06 17:30:20 -0400

    didnt push to heroku?

--- src/server/index.js
+++ src/server/index.js
@@ -75,12 +75,8 @@ console.log('process', process.env.PORT)
   // await new Promise((resolve) =>
   //   httpServer.listen({ port: config.port }, resolve)
   // );
-  await new Promise((resolve) => httpServer.listen({ port: process.env.PORT || 4000 }, resolve).then(({url}) => {
-    console.log(
-      `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
-    );
-  }));
-  
+  await new Promise((resolve) => httpServer.listen({ port: process.env.PORT || 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 
 };
 
